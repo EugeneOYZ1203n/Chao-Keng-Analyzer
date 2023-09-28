97fcfa3406262ff7ca11c6984e720ce71253b7b9
Eugene Oh Yun Zheng <eugeneoyz.12.03.n@gmail.com>
2023-09-29 00:03:31 +0800

    New parser - Can check for MC and Status Msgs

--- chat_parser.py
+++ chat_parser.py
@@ -7,57 +7,18 @@ CHAT_LOG_PATH = Path("./input/_chat.txt")
 
 Chat_log = open(CHAT_LOG_PATH, mode="r", encoding='utf8')
 
-data = defaultdict(list)
-nameList = defaultdict(set)
+## Splitting Chat into individual msgs
+msgs = []
 
+current = ""
 for line in Chat_log:
-    
-    isNotImage = not "image omitted" in line
-    isNotLink = not "http" in line
+    if re.search("\[[0-9]*/[0-9]*/[0-9]*", line):
+        msgs.append(current)
+        current = ""
+    current += line + " "
 
-    if isNotImage & isNotLink:
-        continue
+for i in range(40):
+    msg = msgs[i]
 
-    isStatusMsg = bool(re.search(".*/.*/.*/.*/.*", line))
-    hasDate = bool(re.search("[0-9]*/[0-9]*/[0-9]*", line))
-
-    shortMsg = isStatusMsg & hasDate
-
-    
-
-    if shortMsg:
-        date_str = re.findall('[0-9]*/[0-9]*/[0-9]*', line)[0]
-        status = re.findall('].*/.*/.*', line)[0]
-
-        ## Handling the date
-        day, month, year = date_str.split('/')
-        date_object = datetime(int(year), int(month), int(day))
-
-        ## Day of the week
-        weekday = date_object.weekday()
-
-        ## Status Splitting
-        status = status.lstrip('] ')
-        name1 = re.findall(".*:", status)[0].rstrip(':')
-        name2, location, status_type = re.findall(": .*/.*/.*", status)[0].lstrip(': ').split('/')[:3]
-
-        ## Status Type Identification
-        status_type = status_type.lower()
-
-        if re.search(".*rso .*", status_type):
-            status_type = "RSO"
-        elif re.search(".*ma .*", status_type):
-            status_type = "MA"
-        elif re.search(".*off .*", status_type):
-            status_type = "OFF"
-        elif re.search(".*ll .*", status_type):
-            status_type = "LL"
-        elif re.search(".*ol .*", status_type):
-            status_type = "OL"
-
-        data[name1].append((date_object, location, status_type))
-        nameList[name1].add(name2)
-
-consolidated_info = defaultdict(list)
-
-print(data)
+    isStatusMsg = re.search("\[.*].*:.*/.*/.*", msg)
+    isMCMsg = all([x in msg for x in ["Who", "What", "When", "Where", "Why"]])
